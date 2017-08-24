02a4e102c1ca5311d5ae4d5b21ed9491f98b5ea5
mnurulamri <mnurulamri@gmail.com>
2017-08-24 12:43:50 +0000

    24-08-2017
    View untuk halaman utama yang memuat informasi Petunjuk Penggunaan berikut Syarat dan Kententuan

---
+++ Views/home/halamanUtama.php
@@ -0,0 +1,170 @@
+<div style="text-align:center"> <h4>Selamat Datang <?=$nama?> di Sistem Informasi Peminjaman Ruang</h4> </div>
+<div style="text-align:center">Fakultas Ilmu Sosial dan Ilmu Politik Universitas Indonesia</div>
+<div style="text-align:center">
+<?php
+//$photo = 'data:image/png;base64,'.$foto;
+//echo "<img src = '$photo' width='100px'' height='150px' />";
+?>
+</div>
+
+<div class="box box-info">
+	<div class="box-header with-border" style="text-align:center">
+		<h3 class="box-title"><div style="color:#428bca">Petunjuk Penggunaan</div></h3>
+		<div class="box-tools pull-right">
+			<!-- Buttons, labels, and many other things can be placed here! -->
+			<!-- Here is a label for example -->			
+			<span class="label label-primary">!</span>
+		</div><!-- /.box-tools -->
+	</div><!-- /.box-header -->
+	<div class="box-body">
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Peminjaman dan penggunaan ruangan dilakukan oleh user, yaitu perwakilan user seluruh Unit Kerja dan telah ditunjuk oleh Unit masing-masing (PAF/DEPT/PROG/HM).
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Peminjaman dan penggunaan ruangan oleh user yang sudah terdaftar dapat dijalankan dengan mengakses website http://ppf.fisip.ui.ac.id menggunakan login akun SSO UI.
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				User yang akan melakukan peminjaman dan penggunaan ruangan, agar mengisi menu "Data Pengguna". (Fitur ini berguna untuk membantu user agar tidak menginput data pemohon berulang kali pada saat pengisian form permohonan peminjaman ruang).
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Pengecekan peminjaman dan penggunaan ruangan agar tidak bentrok dengan user lainnya dapat dilakukan dengan mengakses menu "Cek Ruang Kosong".
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				
+				Pengecekan ruangan yang ada memiliki kriteria dan definisi penggunaan sebagai berikut :
+				<ul>
+					<li>
+						Ruang Kelas (Ruang yang digunakan untuk kegiatan perkuliahan)
+					</li>
+					<li>
+						Ruang Non Kelas (Ruang yang digunakan untuk kegiatan diluar perkuliahan)
+					</li>
+				</ul>
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Pengecekan ruangan yang tersedia bisa dicari dengan penelusuran berdasarkan nama ruangan maupun waktu penggunaan melalui menu yang tersedia "Cek Ruang Kosong" 
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Untuk mengajukan permohonan peminjaman ruang, user dipersilahkan mengisi terlebih dahulu form permohonan peminjaman dengan menjalankan menu "Pengajuan Baru"
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Pengisian data pada form permohonan peminjaman tidak akan terproses dan tersimpan apabila ruangan yang akan digunakan sudah diisi oleh pengguna yang lain (jadwal bentrok).
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Data pengajuan peminjaman dan penggunaan ruang secara otomatis bisa dilihat pada daftar pengajuan dan user masih memiliki kesempatan melakukan perubahan sebelum form pengajuan dicetak.
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Data pengajuan peminjaman dan penggunaan ruangan yang sudah valid dapat dicetak dengan mengklik icon "pdf" pada data pengajuan.
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Status dan proses validasi dan verifikasi peminjaman permohonan dapat dilihat melalui menu "Status peminjaman" yang terdiri dari:
+				<ul>					
+					<li>Menunggu Persetujuan Koordinator PPF</li>
+					<li>Menunggu Persetujuan Wakil Manajer PPF</li>
+					<li>Disetujui</li>
+			</ul>
+			</p>
+		</div>
+		<div class="callout" style="padding:2px">
+			<p>
+				Peminjaman dan penggunaan ruangan ruang baru bisa dilakukan apabila status peminjaman sudah disetujui.
+			</p>
+		</div>
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Informasi seluruh peminjaman dan penggunaan ruangan yang sudah dilakukan oleh user dan telah kami proses bisa dilihat dimenu "Status Peminjaman".
+			</p>
+		</div>
+	</div><!-- /.box-body -->
+	<!--<div class="box-footer pull-right">
+	PPF FISIP UI
+	</div> box-footer -->
+</div><!-- /.box -->
+<div class="box box-warning">
+	<div class="box-header with-border" style="text-align:center">
+		<h3 class="box-title"><div style="color:#cc8800">Syarat dan Ketentuan</div></h3>			
+		<div class="box-tools pull-right">
+			<!-- Buttons, labels, and many other things can be placed here! -->
+			<!-- Here is a label for example -->
+			<span class="label label-warning">!</span>
+		</div><!-- /.box-tools -->
+	</div><!-- /.box-header -->
+	<div class="box-body">
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Permohonan peminjaman dan penggunaan ruang hanya bisa diproses selambat-lambatnya 1 (satu) hari sebelum kegiatan diselenggarakan.
+			</p>		
+		</div>
+
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Kegiatan yang tidak jadi terlaksana/batal agar segera melakukan konfirmasi kepada petugas (Sekretariat PPF)
+			</p>	
+		</div>
+
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Kegiatan yang dilaksanakan pada hari sabtu hanya diizinkan sampai dengan pukul 14.00 WIB
+			</p>	
+		</div>
+
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Kegiatan yang dilaksanakan pada hari Senin s/d Jumat hanya diizinkan sampai dengan pukul 21.00 WIB.
+			</p>		
+		</div>
+
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Penggunaan ruang di gedung E dan H hanya diprioritaskan untuk kegiatan perkuliahan.
+			</p>		
+		</div>
+
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Dalam rangka kepentingan fakultas, penyelenggaraan kegiatan di ruang rapat Nurani, AJS dan F.201/202 sewaktu-waktu dapat dibatalkan  atau dipindakan
+			</p>		
+		</div>
+
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Kegiatan yang diselenggarakan dan dilaksanakan diluar kuliah agar melampirkan surat izin kegiatan dari Unit Kerja Kemahasiswaan dan unit Kerja Ventura.
+			</p>		
+		</div>
+
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Kegiatan dan penggunaan fasilitas lapangan basket dan futsal tidak diizinkan digunakan dalam waktu yang bersamaan. 
+			</p>		
+		</div>
+
+		<div class="callout" style="padding:5px; background:#f8f9fa">
+			<p>
+				Penyelenggaraan mata kuliah pengganti dari pukul 08.00 s/d 16.00 WIB, permohonan pengajuan dan penggunaan ruang kelas hanya diproses dan dilakukan oleh Unit PPAA.
+			</p>		
+		</div>
+
+	</div>
+	
+</div>
